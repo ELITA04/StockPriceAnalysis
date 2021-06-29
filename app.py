19e25f68f36dc556847facdea0c6a03fe2f67929
ELITA04 <elita041999@gmail.com>
2021-06-29 15:07:16 +0530

    Made a few minor changes

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import streamlit as st
 import plotly.graph_objects as go
 import pandas_datareader.data as web
 
-def plot_high_low(options, start_date, end_date):
+def plot_high(options, start_date, end_date):
     """
-    Plotting the high and low prices
+    Plotting the high prices
 
     Parameters:
         option (list): List containing the names of the company
@@ -15,12 +15,30 @@ def plot_high_low(options, start_date, end_date):
         end_date : Date uptil which you want 
 
     """
-    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM'}
+    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM', 'Microsoft' : 'MSFT', 'Apple' : 'AAPL'}
     fig = go.Figure()
     for opt in options:
         df = pd.DataFrame(web.DataReader(d[opt], 'yahoo', start_date, end_date)) 
         fig.add_trace(go.Scatter(x=df.index.get_level_values('Date'), y = df['High'], mode = 'lines', name = f'{opt}'))
-    fig.update_layout(title = f"Daily Stock Highest Prices ", yaxis_title = 'Stock Prices', xaxis_title = 'Date')
+    fig.update_layout(title = "Daily High Price", yaxis_title = 'Stock Prices', xaxis_title = 'Date')
+    st.plotly_chart(fig)
+
+def plot_close(options, start_date, end_date):
+    """
+    Plotting the close prices daily
+
+    Parameters:
+        option (list): List containing the names of the company
+        start_date : Date from which you want 
+        end_date : Date uptil which you want 
+
+    """
+    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM', 'Microsoft' : 'MSFT', 'Apple' : 'AAPL'}
+    fig = go.Figure()
+    for opt in options:
+        df = pd.DataFrame(web.DataReader(d[opt], 'yahoo', start_date, end_date)) 
+        fig.add_trace(go.Scatter(x=df.index.get_level_values('Date'), y = df['Close'], mode = 'lines', name = f'{opt}'))
+    fig.update_layout(title = "Daily Close Price", yaxis_title = 'Stock Prices', xaxis_title = 'Date')
     st.plotly_chart(fig)
 
 def plot_volume_traded(options, start_date, end_date):
@@ -33,13 +51,13 @@ def plot_volume_traded(options, start_date, end_date):
         end_date : Date uptil which you want 
 
     """
-    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM'}
+    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM', 'Microsoft' : 'MSFT', 'Apple' : 'AAPL'}
     fig = go.Figure()
 
     for opt in options:
         df = pd.DataFrame(web.DataReader(d[opt], 'yahoo', start_date, end_date))
         fig.add_trace(go.Scatter(x=df.index.get_level_values('Date'), y = df['Volume'], mode = 'lines', name = f'{opt}'))
-    fig.update_layout(title = f"Volume Traded", yaxis_title = 'Volume', xaxis_title = 'Date')
+    fig.update_layout(title = f"Daily Volume Traded", yaxis_title = 'Volume', xaxis_title = 'Date')
     st.plotly_chart(fig)
 
 def plot_cumulative_return(options, start_date, end_date):
@@ -51,14 +69,14 @@ def plot_cumulative_return(options, start_date, end_date):
         start_date : Date from which you want 
         end_date : Date uptil which you want 
     """
-    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM'}
+    d = {'Google' : 'GOOGL',  'Facebook' : 'FB', 'Tesla' : 'TSLA', 'Ford' : 'F', 'GM' : 'GM', 'Microsoft' : 'MSFT', 'Apple' : 'AAPL'}
     fig = go.Figure()
     for opt in options:
         df = pd.DataFrame(web.DataReader(d[opt], 'yahoo', start_date, end_date)) 
         df['returns'] = (df['Close']/df['Close'].shift(1)) - 1
         df['Cumulative Return'] = (1 + df['returns']).cumprod()
         fig.add_trace(go.Scatter(x=df.index.get_level_values('Date'), y = df['Cumulative Return'], mode = 'lines', name = f'{opt}'))
-    fig.update_layout(title = 'Cumulative Return ', yaxis_title = 'Stock Prices', xaxis_title = 'Date')
+    fig.update_layout(title = "Cumulative Return", yaxis_title = 'Stock Prices', xaxis_title = 'Date')
     st.plotly_chart(fig)
 
 
@@ -68,11 +86,13 @@ def main():
     end_date = st.sidebar.date_input('End date', datetime.datetime.now())
     option = st.multiselect(
         'Company Name',
-        ['Google', 'Facebook', 'Tesla', 'Ford', 'GM']
+        ['Google', 'Facebook', 'Tesla', 'Ford', 'GM', 'Microsoft', 'Apple']
     )
 
     if option is not None:
-        plot_high_low(option, start_date, end_date)
+        plot_high(option, start_date, end_date)
+
+        plot_close(option, start_date, end_date)
 
         plot_volume_traded(option, start_date, end_date)
 
